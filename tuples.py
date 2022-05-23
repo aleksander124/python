ed842bf757674349f233cebe686ff2226483fe31
aleksander124 <olerandox@gmail.com>
2022-05-23 13:50:26 +0200

    Add file tuples.py

---
+++ tuples.py
@@ -0,0 +1,13 @@
+my_list = ["jody", "susan", "mario", False]
+my_list.append("bob")
+#print(my_list)
+
+x = 6
+my_tuple = ("apple", "banana", "grapes", 5, x)
+# You can't be changed -> you cant add anything -> locked
+# For example you can use it to define variables that dont change
+red = (255, 0, 0)
+
+# You can easly access to some values from ur tuple
+print(red[0])
+print(red)
