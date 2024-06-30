2032c009db8982568331abac67a320f211ef9952
Aleksander Okoń <olerandox@gmail.com>
2024-06-30 17:41:12 +0200

    fix Possible binding to all interfaces

--- cmdb-api/main.py
+++ cmdb-api/main.py
@@ -11,8 +11,8 @@ app = FastAPI(
     description="Api based on postgress database created to store informations about virtualmachines and users",
     version="1.0.0",
     docs_url="/docs",  # Swagger UI endpoint
-    redoc_url="/redoc",         # ReDoc endpoint
-    openapi_url="/openapi.json" # OpenAPI schema endpoint
+    redoc_url="/redoc",  # ReDoc endpoint
+    openapi_url="/openapi.json"  # OpenAPI schema endpoint
 )
 
 # Include API router with a prefix if necessary
@@ -21,4 +21,5 @@ app.include_router(api_router, prefix="/api", tags=["api"])
 # Run the application
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
