8e0522d0959c8d1052626d7732f27086b6cad3de
Aleksander Okoń <aleksander.okon@medicover.pl>
2024-08-01 15:57:05 +0200

    delete all files

--- fastapi/app/dependencies.py
+++ fastapi/app/dependencies.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-from .models.user import Base
 
-DATABASE_URL = "postgresql://user:password@localhost/logowanie"
+# Update this URL with your external database connection details
+DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/logowanie"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
