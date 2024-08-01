b443e1af0418c2b33aeb2419bac2ce42edb1d906
Aleksander Okoń <aleksander.okon@medicover.pl>
2024-08-01 16:21:26 +0200

    add fastapi docs custom configuration

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -5,6 +5,15 @@ from .database import engine, Base
 # Create the database tables (if needed, typically run once to sync schema)
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Login php application api",
+    description="This is login site api.",
+    version="1.0.0",
+    openapi_url="/api/openapi.json",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
 
 app.include_router(users.router, prefix="/api")
+
+app = FastAPI()
