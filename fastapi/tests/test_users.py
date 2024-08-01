33bba79791787e5267e27701c2087b2114546bc9
Aleksander Okoń <aleksander.okon@medicover.pl>
2024-08-01 16:58:52 +0200

    remove tests directory

--- fastapi/tests/test_users.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-def test_create_user():
-    response = client.post("/users/", json={"user": "testuser", "passw": "testpassword", "email": "test@example.com"})
-    assert response.status_code == 200
-    assert response.json()["user"] == "testuser"
